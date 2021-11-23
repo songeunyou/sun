1946094c001d52355a8ab8bc5dca7c0d66132060
song <songeun.you7@gmail.com>
2021-11-23 15:21:51 -0500

    Calibrate colors for sky and sun

--- src/App.js
+++ src/App.js
@@ -16,33 +16,44 @@ function App() {
   }, []);
 
   function handleMove(e) {
-    // console.log(e.clientX, e.clientY);
     let x = Math.floor(255 * (e.clientX / window.innerWidth));
     let y = Math.floor(255 * (e.clientY / window.innerHeight));
 
+    let h = 205 + Math.floor(35 * (e.clientY / window.innerWidth));
+    let s = 95 - Math.floor(40 * (e.clientY / window.innerWidth));
+    let l = 70 - Math.floor(80 * (e.clientY / (window.innerHeight + 200)));
+
+    if (h < 160 && h > 65) {
+      h = 65;
+    }
+
     setSky({
-      position: 'relative',
-      backgroundColor: `rgb(${0}, ${x}, ${y})`
+      backgroundColor: `hsl(${h}, ${s}%, ${l}%)`
     });
+
+    let sun_h = 40 - Math.floor(40 * (e.clientY / window.innerWidth));
+    let sun_s = 100;
+    let sun_l = 100 - Math.floor(40 * (e.clientY / window.innerHeight));
 
     setSun({
-      position: 'absolute',
-      width: '30px',
-      height: '30px',
       top: e.clientY,
       left: e.clientX,
-      backgroundColor: `rgb(${y}, ${x}, ${200})`,
-      borderRadius: '20px'
+      backgroundColor: `hsl(${sun_h}, ${sun_s}%, ${sun_l}%)`,
+      boxShadow: `0px 0px 15px hsla(${sun_h}, ${sun_s}%, ${sun_l}%,0.6), 0px 0px 6px hsla(${sun_h}, ${sun_s}%, ${sun_l}%,0.6), 0px 0px 2px hsla(${sun_h}, ${sun_s}%, ${sun_l}%,0.6)`
     });
   }
-
-  // console.log(sun);
 
   return (
     <div className="App">
       <div id="sky" style={sky}>
         <p>{sky.backgroundColor}</p>
-        {/* <p>coordinates: {x}, {y}</p>*/}
+        <p>{sun.backgroundColor}</p>
+
+        <div className="horizon-fade"></div>
+        <div className="sunrise-red-filter"></div>
+        <div className="sunrise-glow"></div>
+        <div className="sunrise-light"></div>
+
         <div id="sun" style={sun}></div>
       </div>
     </div>
